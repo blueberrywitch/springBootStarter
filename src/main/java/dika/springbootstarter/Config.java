efd1ef720cbe653182e21a95d9d149ee78f63a9c
DinaraM <blueberrywitch@yandex.ru>
2025-02-01 21:03:14 +0300

    add files

--- src/main/java/dika/springbootstarter/Config.java
+++ src/main/java/dika/springbootstarter/Config.java
@@ -1,2 +1,21 @@
-package dika.springbootstarter;public class Config {
+package dika.springbootstarter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class Config {
+
+    RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public IncomeClient incomeClient(RestTemplate restTemplate) {
+        return new IncomeClient(restTemplate);
+    }
 }
--- src/main/java/dika/springbootstarter/IncomeClient.java
+++ src/main/java/dika/springbootstarter/IncomeClient.java
@@ -1,4 +1,27 @@
 package dika.springbootstarter;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
 public class IncomeClient {
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public IncomeClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public <T> List<T> getData(ParameterizedTypeReference<List<T>> typeReference, String baseUrl) {
+        return restTemplate.exchange(
+                baseUrl, HttpMethod.GET,
+                null,
+                typeReference)
+                .getBody();
+    }
+
 }
